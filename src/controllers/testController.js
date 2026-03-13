e7e4877a02aef55e3176ed0156e561e6f2c1fa2a
applandsys <applandsys@gmail.com>
2026-03-13 18:58:59 +0600

    url show

--- src/controllers/testController.js
+++ src/controllers/testController.js
@@ -5,7 +5,8 @@ exports.testDB = async (req, res) => {
     try {
         const res = await pool.query("SELECT NOW()");
        // console.log(res.rows)
-        res.json(res.rows);
+        res.json({response:res.rows ,
+        url: process.env.DATABASE_URL});
 
     } catch (error) {
         res.status(500).json(error.message);
